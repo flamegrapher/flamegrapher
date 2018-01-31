ee93166526e3d3fd841a1fe98881e5493aab9dcc
Leonardo Freitas Gomes <leonardo.f.gomes@gmail.com>
2018-02-01 00:35:44 +0100

    Todo

--- src/main/java/flamegrapher/backend/JavaFlightRecorder.java
+++ src/main/java/flamegrapher/backend/JavaFlightRecorder.java
@@ -19,6 +19,17 @@ public class JavaFlightRecorder implements Profiler {
         Process process = Process.spawn(vertx, "jcmd");
         StringBuilder str = new StringBuilder();
         
+        // TODO
+        // Define a template for processing the output and converting to string
+        // The other commands basically call stuff without much output
+        // Need to handle exceptions
+        // Need to check errors from the output of the program to send back a msg
+        // Need to read the JFR file in a non-blocking way
+        // Integrate code of JFR to flame graph
+        // Check if we could base the parsing logic on the the JFR coming with Java 9. Can we generate an executable?
+        // Check http://hirt.se/blog/?p=920
+        // Check https://steveperkins.com/using-java-9-modularization-to-ship-zero-dependency-native-apps/
+        
         process.stdout()
                .handler(buf -> {
                    str.append(buf);
