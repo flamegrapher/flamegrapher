99d273665a8b23836d033fbd01924614df07411c
Leonardo Freitas Gomes <leonardo.f.gomes@gmail.com>
2018-04-10 17:07:15 +0200

    Handling flamegraph search and highlight

--- frontend/src/Flames.vue
+++ frontend/src/Flames.vue
@@ -1,17 +1,21 @@
 <template>
     <div>
         <b-row align-h="start">
-            <b-col cols="6">
-                <b-input-group>
-                    <b-input-group-prepend>
-                        <b-btn size="sm" @click="search()">Search</b-btn>
-                    </b-input-group-prepend>
-                    <b-form-input size="sm"></b-form-input>
-                    <b-button-group>
-                        <b-btn size="sm" variant="link" @click="reset()">Reset zoom</b-btn>
-                        <b-btn size="sm" variant="link" @click="clear()">Clear</b-btn>
-                    </b-button-group>
-                </b-input-group>
+            <b-col cols="8">
+                <b-form size="sm" inline @submit="search()" @reset="clear()">
+                    <b-input-group>
+                        <b-input-group-prepend>
+                            <b-btn size="sm" @click="search()">Search</b-btn>
+                        </b-input-group-prepend>
+
+                        <b-form-input size="sm" type="text" v-model="searchExpression"></b-form-input>                    
+                        
+                        <b-input-group-append>
+                            <b-btn size="sm" variant="link" @click="reset()">Reset zoom</b-btn>
+                            <b-btn size="sm" variant="link" @click="clear()">Clear</b-btn>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-form>
             </b-col>
         </b-row>
         <b-row>
@@ -34,17 +38,19 @@ export default {
   name: "flames",
   data () {
     return {
-      chartState: []
+      chartState: [],
+      searchExpression: ""
     };
   },
   methods: {
-    search: function (text) {
-      this.chartState.search(text);
+    search: function () {
+      this.chartState.search(this.searchExpression);
     },
     reset: function () {
       this.chartState.resetZoom();
     },
     clear: function () {
+      this.searchExpression = "";
       this.chartState.clear();
     }
   },
