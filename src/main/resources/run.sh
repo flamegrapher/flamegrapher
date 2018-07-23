1cb113d4eea9a505d2c022b84190f1ef3172f616
Emmanuel <emmanuel.gire@conztanz.com>
2018-07-23 15:06:20 +0200

    FLAMEGRAPHER_HTTP_PORT override capability using environment property

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
+
+# Default values
+HTTP_PORT=8080
+while getopts 'p:' option; do
+  case "$option" in
+	p) HTTP_PORT=$OPTARG
+	   printf "Http port : $OPTARG\n"
+	   ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       exit 1
+       ;;
+  esac
+done
+
+export FLAMEGRAPHER_HTTP_PORT=$HTTP_PORT
+
 # This script will be filtered, copied to this project basedir, and made executable
 java -jar target/flamegrapher-${version}-fat.jar
